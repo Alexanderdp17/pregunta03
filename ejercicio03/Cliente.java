44b0b524ca1eb872427e07454b62aa352f6f2d58
Alexanderdp17 <104165148+Alexanderdp17@users.noreply.github.com>
2022-05-23 21:41:41 -0500

    Add files via upload

---
+++ ejercicio03/Cliente.java
@@ -0,0 +1,10 @@
+package ejercicio03;
+
+public class Cliente
+{
+    String nombres;
+    String apepat;
+    String apemat;
+    int dni;
+
+}
---
+++ ejercicio03/ClienteControlador.java
@@ -0,0 +1,9 @@
+package ejercicio03;
+
+
+public class ClienteControlador
+{
+    //import.Cliente
+    int contador;
+
+}
